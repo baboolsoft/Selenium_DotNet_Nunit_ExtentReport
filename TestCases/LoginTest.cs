dc436933efee91f59e5a59bfebc3be88e932965f
Angapparaja <88724733+Angapparaja@users.noreply.github.com>
2022-11-15 15:03:46 +0530

    First commit

---
+++ TestCases/LoginTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using SeleniumNunitExtentReport.Config;
+using SeleniumNunitExtentReport.PageMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumNunitExtentReport.TestCases
+{
+    [TestFixture]
+    public class LoginTest : ReportGenerationClass
+    {
+        LoginPage loginPage;
+        [Test]
+        [Category("Login")]
+        public void test_validLogin()
+        {
+            loginPage = new LoginPage(GetDriver());
+            loginPage.goToPage();
+            loginPage.goToToggleMenu();
+            loginPage.goToLoginMenu();
+            loginPage.enterUserName("John Doe");
+            loginPage.enterPassword("ThisIsNotAPassword");
+            loginPage.clickLoginBtn();
+            Assert.IsTrue(loginPage.verifyDashboard());
+            loginPage.closeBrowser();
+        }
+
+        [Test]
+        [Category("Login")]
+        public void test_invalidLogin()
+        {
+            loginPage = new LoginPage(GetDriver());
+            loginPage.goToPage();
+            loginPage.goToToggleMenu();
+            loginPage.goToLoginMenu();
+            loginPage.enterUserName("John Doe");
+            loginPage.enterPassword("ThisIsNotA");
+            loginPage.clickLoginBtn();
+            Assert.IsTrue(loginPage.verifyDashboard());
+            loginPage.closeBrowser();
+        }
+    }
+}
